1fe8efb0a832fb46d705d912f5a01b2f60da9058
rfilgueras <68621948+rfilgueras@users.noreply.github.com>
2020-12-18 00:22:21 -0600

    Update Terms.cs

--- Terms.cs
+++ Terms.cs
@@ -1,28 +1,126 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
 using static System.Console;
-namespace StudyApplication
+using System.IO;
+
+namespace DataStudyApplication
 {
-    public class Terms
+    class Terms
     {
-        public string Term { get; set; }
-        public string Definition { get; set; }
+        public static string DataFile = "terms.txt";
+        public static string Content = "(Empty File)";
+        public static string Term = "terms.txt";
+        public static string Definitions = "defintions.txt";
 
-        public static void TermItems ()
+        public static List<string> TermName = new List<string>();
+        public static List<string> TermDefinition = new List<string>();
+
+        public static void Check()
         {
-            string[] Term = new string[5];
-            Term[0] = "class\n - have attributes(properties) and operations(functions)";
-            Term[1] = "System\n - a namespace that contains commonly used classes";
-            Term[2] = "Console\n - a class of standard input, output and error streams foor Console application";
-            Term[3] = "Dot Notation\n - used to refernce something from the library in the hierarchy path ans is expressed in code with dots between elements";
-            Term[4] = "Class Diagram\n - describes a template for creating multiple versions of something";
-
-            for (int i = 0; i < Term.Length; i++)
+            if (File.Exists(DataFile))
             {
-                string termitems = $"{Term[i]}";
-                Console.WriteLine(termitems);
+                Content = File.ReadAllText(DataFile);
             }
 
+            Console.WriteLine(Content);
+        }
 
+        public static void UpdateTerms()
+        {
+            if (File.Exists(Term))
+            {
+                using (StreamReader sr = File.OpenText(Term))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        TermName.Add(s);
+                    }
+                }
+            }
+
+            if (File.Exists(Definitions))
+            {
+                using (StreamReader sr = File.OpenText(Definitions))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        TermDefinition.Add(s);
+                    }
+                }
+            }
+        }
+
+        // Will display choices for the player to choose from and checks if the answer is correct
+        public static void DisplayChoice()
+        {           
+            // Will display the definition from the definitions text file as a choice for user.
+            for (int i = 0; i < TermName.Count; i++)
+            {
+                WriteLine($"{i}: {TermName[i]}");
+               
+            }
+        }        
+
+        public static void DisplayQuestion()
+        {
+            UpdateTerms();
+            // Will display the question for every term in the text file.
+            for (int i = 0; i < TermDefinition.Count; i++)
+            {                
+                ForegroundColor = ConsoleColor.Blue;
+
+                WriteLine($"\n{TermDefinition[i]}?\n");
+                ResetColor();
+                DisplayChoice();
+                String userAnswer = Utility.TryAnswer();
+                // results userAnswer to an int
+                int input = Int32.Parse(userAnswer);
+                if (i == input)
+                {
+                    Utility.CorrectNotification();
+                    Player.ScoreTraker();
+                }
+                else
+                {
+                    Utility.IncorrectNotification();
+                }
+                Utility.Continue();
+                Clear();               
+            }
+        }
+
+
+        // If player receives a certain amount of points after the game is over
+        public static void EndGameMessage()
+        {
+            if (TermName.Count >= 15)
+            {
+                if (Player.Score <= 6)
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("Thats too bad!");
+                    ResetColor();
+                    Utility.PlayAgain();
+
+                }
+                if (Player.Score >= 10 && Player.Score != 10)
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("\n Wow what a high score!");
+                    ResetColor();
+                    Utility.PlayAgain();
+                }
+                if (Player.Score == 15)
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("Congratulations! You just got a perfect Score!");
+                    ResetColor();
+                    Utility.PlayAgain();
+                }
+            }
         }
     }
 }
